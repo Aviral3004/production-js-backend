3a0928ba817f559112d881d2068fbf13d7503bfd
Aviral Kaushal <guest3@Avirals-MacBook-Pro.local>
2024-05-21 08:36:38 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -27,4 +27,14 @@ app.use(express.static("public")) // this will be used to store our files/pdfs/i
 
 app.use(cookieParser())
 
+//! routes will start from here
+
+import userRouter from "./routes/user.routes.js"
+
+//! routes declaration
+
+// http://localhost:8000/api/v1/users/<routes_in_user.routes.js>
+app.use("/api/v1/users", userRouter) // we used a middleware to transfer the control to userRouter in user.routes.js
+//! now we can write our routes in the controller user.routes.js
+
 export default app
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,16 +7,16 @@
 // and after a middleware completes its task with the help of next
 // it will go to the next middleware
 
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message,
-//         })
-//     }
-// }
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+}
 
 export {asyncHandler}
 
